be25463e813c8cd466006234008c2f8b2d49422b
giovanni-agra <64974757+giovanni-agra@users.noreply.github.com>
2021-05-19 03:37:17 +0700

    Editing some final things for the backend.

--- MedicalRecordsProject/urls.py
+++ MedicalRecordsProject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('account/', include('MedRec.urls')),
+    path('user/', include('MedRec.urls')),
     path('', include('MedRecAppointments.urls')),
 ]
